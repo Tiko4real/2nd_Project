394d63550ea337f2cbcf6673ad085cdd452e55c3
Taim <e02taitfi@gmail.com>
2024-06-16 17:15:03 +0200

    addeventlistener to script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const welcomePage = document.getElementById('welcome-page');
+    const rulesModal = document.getElementById('rules-modal');
+    const usernamePage = document.getElementById('username-page');
+    const gamePage = document.getElementById('game-page');
+    con;
+   
